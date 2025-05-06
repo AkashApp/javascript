c345fa21281dba508dadff9dd4f4cc73b6961aba
akash-borate <27akashborate@gmail.com>
2025-05-06 16:51:43 +0530

    functions and its type

---
+++ functions.js
@@ -0,0 +1,15 @@
+// Functions
+// function is a block of code that can be reused
+
+// what is function declaration
+// function declaration is a way to define a function in javascript
+
+// what is function expression
+// when you store a function inside a variable it is called function expression
+
+// what are first class functions?
+//  first class functions are functions that can be assigned to a variable
+//  first class functions are functions that can be passed as arguments to other functions
+
+// what is IIFE?
+// IIFE is immediately invoked function expression
