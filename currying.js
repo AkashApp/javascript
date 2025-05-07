f28773d136fda54a7de5e2311220b205011206ab
akash-borate <27akashborate@gmail.com>
2025-05-07 17:16:50 +0530

    currying

---
+++ currying.js
@@ -0,0 +1,8 @@
+// currying
+// currying is a technique in javascript where a function is returned from another function
+// currying is a function which takes one argument at a time and returns a new function
+//  expecting the next argument
+// it is a conversion of functions from callable as this f(a,b)
+//  to callable as that f(a)(b)
+// curried functions are constructed by chaining closures by immediately
+//  returning there inner functions symmetrically
