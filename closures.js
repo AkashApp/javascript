f3980b84d5c53d90ca824b41a6f0ae130c2cc295
akash-borate <27akashborate@gmail.com>
2025-05-07 16:37:10 +0530

    once polyfill

--- closures.js
+++ closures.js
@@ -130,3 +130,22 @@ isSubscribed(); // Already Subscribed and liked
 isSubscribed(); // Already Subscribed and liked
 
 // ------------------------------------------------------------------------------------
+
+// once polyfill
+
+function once(func, context) {
+    let ean;
+    return function () {
+        if(func){
+            ran = func.apply(context || this, arguments);
+            func = null;
+        }
+        return ran;
+    };
+}
+const hello = once(() => console.log("hello"));
+hello();
+hello();
+hello();
+
+// ------------------------------------------------------------------------------------
