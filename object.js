fd6688fa00e7c3ece204830eeb1e24b508f53ff1
akash-borate <27akashborate@gmail.com>
2025-05-09 15:42:09 +0530

    object defination

---
+++ object.js
@@ -0,0 +1,5 @@
+// Objects
+
+// An object is a collection of properties, and property is an association between a name (or Key) and value.
+// A property's value can be a function, in which case the property is called a method.
+
