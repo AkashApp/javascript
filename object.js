f1dad8c95730aead195689f061119389790fd36d
akash-borate <27akashborate@gmail.com>
2025-05-09 16:00:07 +0530

    JSON stringify parse

--- object.js
+++ object.js
@@ -3,3 +3,11 @@
 // An object is a collection of properties, and property is an association between a name (or Key) and value.
 // A property's value can be a function, in which case the property is called a method.
 
+// What's JSON.stringify and JSON.parse?
+
+const user = {
+    name: "Akash",
+    age: 20,
+};
+const strObj = JSON.stringify(user);
+console.log(JSON.parse(strObj));
