0a952fcf64a2971c36207349800c589c14e87571
akash-borate <27akashborate@gmail.com>
2025-05-11 16:03:51 +0530

    apply

--- callApplyBind.js
+++ callApplyBind.js
@@ -2,7 +2,10 @@
 
 var obj = {name:"akash", age:20};
 
-function sayHello(age) {
-    return "Hello " + this.name + " your age is " + age;
+function sayHello(age, proffesion) {
+    return "Hello " + this.name + " your age is " + age + " and he is " + proffesion;
 };
-console.log(sayHello.call(obj,25));
+console.log(sayHello.call(obj,25, "Softerware Engineer"));
+
+// apply
+console.log(sayHello.apply(obj,[25, "Softerware Engineer"]));
